4402070045a5173ff258129e3c23f4e29c40b8eb
EslamKhial1997 <eslamkhial1997@gmail.com>
2026-04-23 22:04:52 +0200

    dynamic url

--- routes/unitRouters.js
+++ routes/unitRouters.js
@@ -1,12 +1,4 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getUnitData,
-  createUnit
-} = require("../controllers/unitController");
-
-router.get("/", getUnitData);
-router.post("/", createUnit);
-
 module.exports = router;
